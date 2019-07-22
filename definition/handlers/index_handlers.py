eaf7e1f637073f32d548e6b6e93572c608e8599f
Julie Anne Moore <jmoore5@unb.ca>
2019-07-22 19:49:44 -0300

    Added a quote to the bottom of the page

--- definition/handlers/index_handlers.py
+++ definition/handlers/index_handlers.py
@@ -1,6 +1,13 @@
 import tornado.web
+import random
 
 from api_adapter import ApiAdapter
+
+quotes = [
+    {"quote": "Few activities are as delightful as learning new vocabulary.", "author(s)": "Tim Gunn"},
+    {"quote": "I collect words--they are sweets in the mouth of sound.", "author(s)": "Sally Gardner, Maggot Moon "}
+]
+
 
 
 class IndexHandler(tornado.web.RequestHandler):
@@ -8,4 +15,5 @@ class IndexHandler(tornado.web.RequestHandler):
         show_links = self.get_argument('show_links', False)
         adapter = ApiAdapter()
         definitions = await adapter.fetch("api/definition")
-        self.render("../views/index.html", definitions=definitions, show_links=show_links)
+        self.render("../views/index.html", definitions=definitions, show_links=show_links, quote=random.choice(quotes))
+
