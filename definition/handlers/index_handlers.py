5ba9b72b7a61e6a376799021ad15a3f2cbbf9f5d
Julie Anne Moore <jmoore5@unb.ca>
2019-07-22 19:28:55 -0300

    Refactored "Edit" button to a module
    - code is a bit messy, but functional
    - should try to cleanup html file

--- definition/handlers/index_handlers.py
+++ definition/handlers/index_handlers.py
@@ -3,14 +3,6 @@ import tornado.web
 from api_adapter import ApiAdapter
 
 
-class EditHandler(tornado.web.RequestHandler):
-    async def get(self):
-        word = self.get_argument('word')
-        adapter = ApiAdapter()
-        definition = await adapter.fetch("api/definition?word=" + word)
-        self.render("../views/edit.html", definition=definition)
-
-
 class IndexHandler(tornado.web.RequestHandler):
     async def get(self):
         show_links = self.get_argument('show_links', False)
